2737e19319ad53f096daf2b02fe1a19718083ee1
Daniel <d.p.murph19@gmail.com>
2018-07-14 11:15:36 -0600

    moved xmonad.hs into .xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,18 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "xmobar /home/dan/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , terminal = "uxterm"
+        }
